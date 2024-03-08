8781aa5a0e0af60fc69ba45e39e5f7e8403a727a
afnaan muztafe <afnaanmuz12@gmail.com>
2024-03-08 08:13:30 -0800

    Add project files.

---
+++ Dictionary app/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Dictionary_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.resultBox = new System.Windows.Forms.TextBox();
+            this.inputButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputBox
+            // 
+            this.inputBox.Font = new System.Drawing.Font("Rockwell", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputBox.Location = new System.Drawing.Point(120, 20);
+            this.inputBox.Multiline = true;
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(346, 37);
+            this.inputBox.TabIndex = 1;
+            this.inputBox.TextChanged += new System.EventHandler(this.inputBox_TextChanged);
+            // 
+            // resultBox
+            // 
+            this.resultBox.BackColor = System.Drawing.SystemColors.GrayText;
+            this.resultBox.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultBox.ForeColor = System.Drawing.Color.White;
+            this.resultBox.Location = new System.Drawing.Point(101, 96);
+            this.resultBox.Multiline = true;
+            this.resultBox.Name = "resultBox";
+            this.resultBox.ReadOnly = true;
+            this.resultBox.Size = new System.Drawing.Size(492, 236);
+            this.resultBox.TabIndex = 2;
+            // 
+            // inputButton
+            // 
+            this.inputButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.inputButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.inputButton.Location = new System.Drawing.Point(487, 18);
+            this.inputButton.Name = "inputButton";
+            this.inputButton.Size = new System.Drawing.Size(96, 39);
+            this.inputButton.TabIndex = 3;
+            this.inputButton.Text = "Search";
+            this.inputButton.UseVisualStyleBackColor = false;
+            this.inputButton.Click += new System.EventHandler(this.inputButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sitka Small", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Word Search;";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Gadugi", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Meaning;";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 405);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputButton);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.inputBox);
+            this.Name = "Form1";
+            this.Text = "Dictionary App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.TextBox resultBox;
+        private System.Windows.Forms.Button inputButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Dictionary app/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dictionary_app
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void inputButton_Click(object sender, EventArgs e)
+        {
+            var my_Dictionary = new Dictionary<string, string>()
+            {
+                {"king" , "Boqor" },
+                {"queen" ,"boqorad" },
+                {"word" ,  "erayga" },
+                {"hot","kulul" },
+                 {"but","lakiin" },
+                  {"can","awood" },
+                   {"Tell","sheeg" },
+                    {"play","Ciyaar" },
+                     {"read","akhrin" },
+                      {"write","qorid" },
+                       {"cook","karin" },
+                        {"large","wayn" },
+                         {"small","yar" },
+                          {"thin","dhuupan" },
+                           {"port","dakad" },
+                            {"act","fall" },
+                             {"change","isbadalka" },
+                              {"house","guri" },
+                               {"picture","sawir" },
+                                {"animal","xayawaan" },
+                                 {"mother","hooyo" },
+                                  {"father","aabo" },
+                                   {"earth","dhul" },
+                                    {"try","izkuday" },
+                                     {"new","cusub" },
+                                      {"work","shaqo" },
+                                       {"self","naf" },
+                                        {"part","qayp" },
+                                         {"one","midh" },
+                                          {"live","nolol" },
+
+            };
+            string word;
+            word = inputBox.Text;
+
+            try
+            {
+                resultBox.Text = my_Dictionary[word];
+            }
+            catch
+            {
+                resultBox.Text = "sorry word not found";
+            }
+        }
+
+        private void inputBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
